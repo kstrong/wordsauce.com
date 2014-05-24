10d1d244114a1ff5300e11342ca98b0965461190
kstrong <kevin@wordsauce.org>
2014-05-24 13:23:44 -0700

    settup up site on sinatra + assetpack

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+ruby '2.1.1'
+
+gem 'compass'
+gem 'foundation'
+
+gem 'sinatra'
+gem 'sinatra-assetpack'
+gem 'sinatra-contrib'
---
+++ app.rb
@@ -0,0 +1,26 @@
+require 'sinatra'
+require 'sinatra/assetpack'
+
+assets do
+  serve '/js', from: 'js'
+  serve '/bower_components', from: 'bower_components'
+
+  js :modernizr, [
+    '/bower_components/modernizr/modernizr.js'
+  ]
+
+  js :libs, [
+    '/bower_components/jquery/dist/jquery.js',
+    '/bower_components/foundation/js/foundation.js'
+  ]
+
+  js :application, [
+    '/js/app.js'
+  ]
+
+  js_compression :jsmin
+end
+
+get '/' do
+  erb :foundation_sample
+end
