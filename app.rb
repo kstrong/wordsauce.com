1d11205707deca9ac5010f7e5734e9c2ccc1f33f
kstrong <kevin@wordsauce.org>
2014-07-09 17:45:42 -0700

    updated show list, use single hash for shows stored in app.rb file

--- app.rb
+++ app.rb
@@ -22,7 +22,30 @@ assets do
   js_compression :jsmin
 end
 
+# Should link to venue and location
+shows = [
+  { date: '7/13/2014', location: 'Ventura, CA', venue: 'Surf Rodeo @ Pierpont Beach', links: ['http://surfrodeo.org'] },
+  { date: '7/29/2014', location: 'San Luis Obispo, CA', venue: 'Frog N Peach', links: [] },
+  { date: '8/1/2014', location: 'Visalia, CA', venue: 'The Cellar Door', links: [] },
+  { date: '8/2/2014', location: 'Ventura, CA', venue: 'Bombay Bar &amp; Grill', links: [] },
+  { date: '8/4/2014', location: 'Santa Monica, CA', venue: 'TRiP', links: [] },
+  { date: '8/7/2014', location: 'Long Beach, CA', venue: 'Liquid Lounge', links: [] },
+  { date: '8/8/2014', location: 'Lomita, CA', venue: 'The Firehouse', links: [] },
+  { date: '8/9/2014', location: 'San Diego, CA', venue: 'TBA', links: [] },
+  { date: '8/13/2014', location: 'Gilroy, CA', venue: 'Studio 222', links: [] },
+  { date: '8/15/2014', location: 'San Jose, CA', venue: 'The Back Bar', links: [] },
+  { date: '8/16/2014', location: 'Half Moon Bay, CA', venue: 'Old Princeton Landing', links: [] },
+  { date: '8/18/2014', location: 'Pacifica, CA', venue: 'Winter\'s Tavern', links: [] },
+  { date: '8/21/2014', location: 'Alameda, CA', venue: 'The Fireside Lounge', links: [] },
+  { date: '8/22/2014', location: 'Eureka, CA', venue: 'The Siren\'s Song Tavern', links: [] },
+  { date: '8/27/2014', location: 'Ashland, OR', venue: 'Club 66', links: [] },
+  { date: '8/28/2014', location: 'Auburn, CA', venue: 'Pistol Petes', links: [] },
+  { date: '8/29/2014', location: 'Ukiah, CA', venue: 'Ukiah Brewing Company', links: [] },
+  { date: '8/30/2014', location: 'Concord, CA', venue: 'Red Hat', links: [] }
+]
+
 get '/' do
+  @shows = shows
   erb :sauce
 end
 
@@ -39,6 +62,7 @@ get '/music' do
 end
 
 get '/tour' do
+  @shows = shows
   erb :tour
 end
 
