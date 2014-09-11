7625b765af5954299861b74b3ebe65a0fc521207
kstrong <kevin@wordsauce.org>
2014-09-10 18:20:18 -0700

    add support for separate tours in show list, added san diego show

--- app.rb
+++ app.rb
@@ -23,33 +23,45 @@ assets do
 end
 
 # Should link to venue and location
-shows = [
-  { date: '7/13/2014', location: 'Ventura, CA', venue: 'Surf Rodeo @ Pierpont Beach', links: ['http://surfrodeo.org'] },
-  { date: '7/25/2014', location: 'Cayucos, CA', venue: 'Cayucos Tavern', links: ['http://www.oldcayucostavern.com/'] },
-  { date: '7/29/2014', location: 'San Luis Obispo, CA', venue: 'Frog N Peach', links: ['http://www.frogandpeachpub.com/'] },
-  { date: '8/1/2014', location: 'Visalia, CA', venue: 'The Cellar Door', links: ['https://www.facebook.com/events/727321980660712/?ref=5'] },
-  { date: '8/2/2014', location: 'Ventura, CA', venue: 'Bombay Bar &amp; Grill', links: ['http://www.bombaybarandgrill.com/'] },
-  { date: '8/4/2014', location: 'Santa Monica, CA', venue: 'TRiP', links: ['http://www.tripsantamonica.com/'] },
-  { date: '8/7/2014', location: 'Long Beach, CA', venue: 'Liquid Lounge', links: ['https://www.facebook.com/pages/Liquid-Lounge/118392788173092'] },
-  { date: '8/8/2014', location: 'Lomita, CA', venue: 'The Firehouse', links: ['https://www.facebook.com/firehouse.lomita'] },
-  { date: '8/9/2014', location: 'San Diego, CA', venue: 'Casa De Snowdrop', links: [] },
-  { date: '8/11/2014', location: 'Anaheim, CA', venue: 'World Famous Doll Hut', links: ['https://www.facebook.com/worldfamousdh'], prtm: true },
-  { date: '8/13/2014', location: 'Gilroy, CA', venue: 'Studio 222', links: ['https://www.facebook.com/studio222productions'] },
-  { date: '8/14/2014', location: 'San Jose, CA', venue: 'The Caravan', links: ['https://www.facebook.com/CaravanLounge'], prtm: true },
-  { date: '8/15/2014', location: 'San Jose, CA', venue: 'The Back Bar', links: ['https://www.facebook.com/backbar408'] },
-  { date: '8/16/2014', location: 'Half Moon Bay, CA', venue: 'Old Princeton Landing', links: ['http://theoldprincetonlanding.com/music-schudule/august-2014/'] },
-  { date: '8/18/2014', location: 'Pacifica, CA', venue: 'Winter\'s Tavern', links: ['http://winterstavern.com/'] },
-  { date: '8/21/2014', location: 'Alameda, CA', venue: 'The Fireside Lounge', links: ['http://www.thefiresidelounge.com/'] },
-  { date: '8/22/2014', location: 'Eureka, CA', venue: 'The Siren\'s Song Tavern', links: ['http://www.thesirenssongtavern.com/'] },
-  { date: '8/23/2014', location: 'Coos Bay, OR', venue: 'Blue Moon Saloon &amp; Cafe', links: ['https://www.facebook.com/pages/Blue-Moon-Saloon-Cafe/464595053656497'] },
-  { date: '8/26/2014', location: 'Medford, OR', venue: 'Johnny B\'s', links: ['https://www.facebook.com/pages/Johnny-Bs/166694203347915'], prtm: true },
-  { date: '8/27/2014', location: 'Ashland, OR', venue: 'Club 66', links: ['https://www.facebook.com/Club66MusicCafe'] },
-  { date: '8/28/2014', location: 'Auburn, CA', venue: 'Pistol Petes', links: ['https://www.facebook.com/pages/Pistol-Petes-Brew-Cue/115203675165310'] },
-  { date: '8/29/2014', location: 'Ukiah, CA', venue: 'Ukiah Brewing Company', links: ['http://ukiahbrewing.com/'] },
-  { date: '8/30/2014', location: 'Concord, CA', venue: 'Red Hat', links: ['https://www.facebook.com/redhatsportsbar'] }
+tours = [
+  {
+    name: 'Sauce or Die Tour 2014',
+    dates: [
+      { date: '7/13/2014', location: 'Ventura, CA', venue: 'Surf Rodeo @ Pierpont Beach', links: ['http://surfrodeo.org'] },
+      { date: '7/25/2014', location: 'Cayucos, CA', venue: 'Cayucos Tavern', links: ['http://www.oldcayucostavern.com/'] },
+      { date: '7/29/2014', location: 'San Luis Obispo, CA', venue: 'Frog N Peach', links: ['http://www.frogandpeachpub.com/'] },
+      { date: '8/1/2014', location: 'Visalia, CA', venue: 'The Cellar Door', links: ['https://www.facebook.com/events/727321980660712/?ref=5'] },
+      { date: '8/2/2014', location: 'Ventura, CA', venue: 'Bombay Bar &amp; Grill', links: ['http://www.bombaybarandgrill.com/'] },
+      { date: '8/4/2014', location: 'Santa Monica, CA', venue: 'TRiP', links: ['http://www.tripsantamonica.com/'] },
+      { date: '8/7/2014', location: 'Long Beach, CA', venue: 'Liquid Lounge', links: ['https://www.facebook.com/pages/Liquid-Lounge/118392788173092'] },
+      { date: '8/8/2014', location: 'Lomita, CA', venue: 'The Firehouse', links: ['https://www.facebook.com/firehouse.lomita'] },
+      { date: '8/9/2014', location: 'San Diego, CA', venue: 'Casa De Snowdrop', links: [] },
+      { date: '8/11/2014', location: 'Anaheim, CA', venue: 'World Famous Doll Hut', links: ['https://www.facebook.com/worldfamousdh'], prtm: true },
+      { date: '8/13/2014', location: 'Gilroy, CA', venue: 'Studio 222', links: ['https://www.facebook.com/studio222productions'] },
+      { date: '8/14/2014', location: 'San Jose, CA', venue: 'The Caravan', links: ['https://www.facebook.com/CaravanLounge'], prtm: true },
+      { date: '8/15/2014', location: 'San Jose, CA', venue: 'The Back Bar', links: ['https://www.facebook.com/backbar408'] },
+      { date: '8/16/2014', location: 'Half Moon Bay, CA', venue: 'Old Princeton Landing', links: ['http://theoldprincetonlanding.com/music-schudule/august-2014/'] },
+      { date: '8/18/2014', location: 'Pacifica, CA', venue: 'Winter\'s Tavern', links: ['http://winterstavern.com/'] },
+      { date: '8/21/2014', location: 'Alameda, CA', venue: 'The Fireside Lounge', links: ['http://www.thefiresidelounge.com/'] },
+      { date: '8/22/2014', location: 'Eureka, CA', venue: 'The Siren\'s Song Tavern', links: ['http://www.thesirenssongtavern.com/'] },
+      { date: '8/23/2014', location: 'Coos Bay, OR', venue: 'Blue Moon Saloon &amp; Cafe', links: ['https://www.facebook.com/pages/Blue-Moon-Saloon-Cafe/464595053656497'] },
+      { date: '8/26/2014', location: 'Medford, OR', venue: 'Johnny B\'s', links: ['https://www.facebook.com/pages/Johnny-Bs/166694203347915'], prtm: true },
+      { date: '8/27/2014', location: 'Ashland, OR', venue: 'Club 66', links: ['https://www.facebook.com/Club66MusicCafe'] },
+      { date: '8/28/2014', location: 'Auburn, CA', venue: 'Pistol Petes', links: ['https://www.facebook.com/pages/Pistol-Petes-Brew-Cue/115203675165310'] },
+      { date: '8/29/2014', location: 'Ukiah, CA', venue: 'Ukiah Brewing Company', links: ['http://ukiahbrewing.com/'] },
+      { date: '8/30/2014', location: 'Concord, CA', venue: 'Red Hat', links: ['https://www.facebook.com/redhatsportsbar'] }
+    ]
+  },
+  {
+    name: 'Fall 2014',
+    dates: [
+      { date: '9/12/2014', location: 'San Diego, CA', venue: 'Gallagher\'s', links: ['http://gallagherspubob.com/'] }
+    ]
+  }
 ]
 
 get '/' do
+  shows = tours.collect { |tour| tour[:dates] }.flatten
   @shows = shows.select { |show| Date.strptime(show[:date], '%m/%d/%Y') >= Date.today }
   erb :sauce
 end
@@ -63,7 +75,7 @@ get '/music' do
 end
 
 get '/tour' do
-  @shows = shows
+  @tours = tours
   erb :tour
 end
 
