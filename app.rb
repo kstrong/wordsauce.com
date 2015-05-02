7191d1ac881463dc6af796084609a2585190032e
kstrong <kevin@wordsauce.org>
2015-05-01 19:50:24 -0700

    correct hoof fest date

--- app.rb
+++ app.rb
@@ -81,7 +81,7 @@ tours = [
       { date: '4/4/2015', location: 'Ventura, CA', venue: 'Bombay Bar &amp; Grill', links: [] },
       { date: '4/24/2015', location: 'Gilroy, CA', venue: 'The Longhouse' },
       { date: '4/25/2015', location: 'Concord, CA', venue: 'Red Hat' },
-      { date: '5/1/2015', location: 'Cayucos, CA', venue: 'Hoof Fest' }
+      { date: '5/2/2015', location: 'Cayucos, CA', venue: 'Hoof Fest' }
     ]
   }
 ]
