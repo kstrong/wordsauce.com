a7ca7ceea35e102a81c1b593e5dbdce801936805
kstrong <kevin@wordsauce.org>
2015-07-27 20:17:59 -0700

    correct bandcamp links on music page, thumb gen script syntax update

--- makethumbs.sh
+++ makethumbs.sh
@@ -1,2 +1,5 @@
 cd public/images/fb_photos
-for filename in *; do convert $filename -resize 100x100\^ -gravity center -extent 100x100 "../thumbs/fb_photos/$filename"; done
+for filename in *
+do
+  convert $filename -resize 100x100\^ -gravity center -extent 100x100 "../thumbs/fb_photos/$filename"
+done
