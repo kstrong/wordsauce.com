d35fe7ff4d8d1fc5fd1affccca37e0ca1579593a
kstrong <kevin@wordsauce.org>
2015-08-05 17:37:35 -0700

    update image sync script to use rsync

--- syncimages.sh
+++ syncimages.sh
@@ -2,9 +2,6 @@
 
 cd public
 find . -name '*.DS_Store' -type f -delete
-tar zcvf images.tar.gz images/*
-scp images.tar.gz wordsauce@wintersfear.com:~/wordsauce.com/public/
-ssh wordsauce@wintersfear.com "cd ~/wordsauce.com/public; tar zxvf images.tar.gz; rm images.tar.gz;"
-rm images.tar.gz
+rsync -av images/ wordsauce:~/wordsauce.com/public/images
 cd -
 
