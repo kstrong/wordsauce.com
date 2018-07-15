77b8e198cb45e87264fb4c5413678acb4f0d8e9b
kstrong <kevin@wordsauce.org>
2018-07-15 02:19:04 -0700

    update littlebig fest details

--- app.rb
+++ app.rb
@@ -206,7 +206,7 @@ tours = [
       { date: '7/3/2018',  location: 'San Luis Obispo, CA', venue: 'SLO Brew', links: ['https://www.slobrew.com/'] },
       { date: '7/4/2018',  location: 'San Francisco, CA', venue: 'Boom Boom Room' },
       { date: '7/8/2018',  location: 'Quincy, CA', venue: 'High Sierra Music Festival', links: ['http://highsierramusic.com/'] },
-      { date: '8/4/2018',  location: 'Whidbey Island, WA', venue: 'TBA' },
+      { date: '8/4/2018',  location: 'Langley, WA', venue: 'Little BIG Fest', links: ['https://www.facebook.com/littlebigfestival'] },
       { date: '8/11/2018', location: 'Morro Bay, CA', venue: 'The Siren', links: ['http://www.ticketfly.com/event/1711681-wordsauce-album-release-party-morro-bay/'] },
       { date: '9/22/2018',  location: 'Visalia, CA', venue: 'Barrelhouse Brewing Co.', links: ['https://www.barrelhousebrewing.com/locations-visalia/'] },
       { date: '11/10/2018',  location: 'Paso Robles, CA', venue: 'Barrelhouse Brewing Co.', links: ['https://www.barrelhousebrewing.com/locations-paso-robles'] },
