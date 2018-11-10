b2bb1a9bb1e1e323a95b875f59494ee64ef66f3f
kstrong <kevin@wordsauce.org>
2018-11-10 00:17:33 -0800

    update left epk photo, move media player functions to app helpers

--- app.rb
+++ app.rb
@@ -25,6 +25,18 @@ assets do
   js_compression :jsmin
 end
 
+helpers do
+  def soundcloud_player(playlist = :theflow)
+    playlist_ids = { :liveinstudio_ep => 2513587, :theleak_ep => 41426186, :theflow => 131843297 }
+    '<iframe width="100%" height="300" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/' + playlist_ids[playlist].to_s + '&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>'
+  end
+
+  def spotify_player(album = :juice)
+    album_ids = { :juice => '0FfEu1jmKOqN4pB868FZgN' }
+    '<iframe src="https://open.spotify.com/embed?uri=spotify:album:' + album_ids[album] + '" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>'
+  end
+end
+
 # Should link to venue and location
 tours = [
   {
